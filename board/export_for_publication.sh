79e268ff80593c974e2234ac2262ca58a556fc41
Brian Lalor <blalor@bravo5.org>
2011-03-25 11:41:25 -0400

    correcting character encoding for parts lst

--- board/export_for_publication.sh
+++ board/export_for_publication.sh
@@ -8,3 +8,7 @@ cd "${board_dir}"
 rm -f iswba.parts.txt iswba.sch.png iswba.brd.png
 
 "${_EAGLE}" -C "script ${board_dir}/export.scr" iswba.brd
+
+## parts list gets exported as iso-8859-1
+iconv -f iso-8859-1 -t utf-8 iswba.parts.txt > iswba.parts.txt.converted
+mv iswba.parts.txt.converted iswba.parts.txt
